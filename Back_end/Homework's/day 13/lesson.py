53c43e3a6d59274b08540797a5591f748438eb19
Sida18 <144269594+Sida18@users.noreply.github.com>
2024-05-08 16:17:47 +0400

    Add files via upload

---
+++ Back_end/Homework's/day 13/lesson.py
@@ -0,0 +1,131 @@
+
+
+
+# for loops
+
+# first homework
+
+print(1)
+print(2) 
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+ 
+
+
+
+# second homework
+
+print(10)
+print(9) 
+print(8)
+print(7)
+print(6)
+print(5)
+print(4)
+print(3)
+print(2)
+print(1)
+
+
+    # third homework
+
+for x in range(0,100):
+        print(x)
+   
+
+   # forth homework
+        for x in reversed(range(1,99)): 
+                  print(x)
+
+
+   # fith homework
+                  
+        
+
+full = 1+2+3+4+5+6+7+8+9+10
+    
+ # sixth homework
+
+
+
+b = 100
+
+a = int(input('enter an number between a,b: '))
+
+for l in range(a,b):
+       
+     
+        print(l)
+    
+
+    # while loops
+        
+    # first homework
+        
+
+print(1)
+print(2) 
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+ # second homework
+
+
+
+
+print(10)
+print(9) 
+print(8)
+print(7)
+print(6)
+print(5)
+print(4)
+print(3)
+print(2)
+print(1)
+
+# third homework
+
+i = 2
+
+while i<=10:
+    print(i)
+    i+=2
+
+
+
+     # forth homework
+
+
+i = 3
+
+while i<=10:
+    print(i)
+    i+=2
+ 
+
+ # fith
+    
+    full = 1+2+3+4+5+6+7+8+9
+
+# sixth
+b = 1
+a = int(input('enter number between a,b: ')) 
+
+while a<=100:
+       print(a)
+       
+       a+=1
